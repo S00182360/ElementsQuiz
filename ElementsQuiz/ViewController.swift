adb8e0fb6e7ee7f24c567609e14f29907e01e516
S00182360 <s00182360@mail.itsligo.ie>
2019-02-05 21:13:15 +0000

    Elements quiz
    end of tasks

--- ElementsQuiz/ViewController.swift
+++ ElementsQuiz/ViewController.swift
@@ -12,9 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    let elementsList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementsList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+    }
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func goToNextElement(_ sender: Any) {
+        currentElementIndex += 1
+        if(currentElementIndex >= elementsList.count){
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementsList[currentElementIndex]
+    }
+    
+    
 }
 
